89e6524e065b42912fd135afed89e9c9f70162c8
Andrey Kharebov <andreykharebov@gmail.com>
2020-10-31 02:38:54 +0300

    Added side sort by menu css

--- src/components/ClothesSection/SideMenu/SideMenu.jsx
+++ src/components/ClothesSection/SideMenu/SideMenu.jsx
@@ -4,7 +4,39 @@ import classes from './SideMenu.module.css';
 function SideMenu() {
   return (
     <div className={classes.sideMenu}>
-      <h2>SIDE MENU</h2>
+      <span>Сортировать по:</span>
+      <ul>
+        <li>
+          <label className={classes.inputContainer}>
+            <input type="radio" name="radio" /> Все
+            <span className={classes.checkmark}></span>
+          </label>
+        </li>
+        <li>
+          <label className={classes.inputContainer}>
+            <input type="radio" name="radio" /> Цене (дорогие)
+            <span className={classes.checkmark}></span>
+          </label>
+        </li>
+        <li>
+          <label className={classes.inputContainer}>
+            <input type="radio" name="radio" /> Цене (дешевые)
+            <span className={classes.checkmark}></span>
+          </label>
+        </li>
+        <li>
+          <label className={classes.inputContainer}>
+            <input type="radio" name="radio" /> Бренду
+            <span className={classes.checkmark}></span>
+          </label>
+        </li>
+        <li>
+          <label className={classes.inputContainer}>
+            <input type="radio" name="radio" /> Категории
+            <span className={classes.checkmark}></span>
+          </label>
+        </li>
+      </ul>
     </div>
   )
 }
